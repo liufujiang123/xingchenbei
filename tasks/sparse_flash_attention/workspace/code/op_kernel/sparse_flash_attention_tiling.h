4c3a67ca06ecf08f70457303a62bd9bf346bec79
Liufujiang <116043508+liufujiang123@users.noreply.github.com>
2026-08-23 09:24:00 +0800

    sfa: add correctness baseline tiling contract

--- tasks/sparse_flash_attention/workspace/code/op_kernel/sparse_flash_attention_tiling.h
+++ tasks/sparse_flash_attention/workspace/code/op_kernel/sparse_flash_attention_tiling.h
@@ -4,5 +4,26 @@
 #include <cstdint>
 
 struct SparseFlashAttentionTilingData {
-    uint32_t length;
+    uint64_t batchSize;
+    uint64_t querySeqLen;
+    uint64_t kvSeqLen;
+    uint64_t queryHeadNum;
+    uint64_t sparseSize;
+    uint64_t totalRows;
+
+    uint32_t queryDtype;
+    uint32_t keyDtype;
+    uint32_t valueDtype;
+    uint32_t queryRopeDtype;
+    uint32_t keyRopeDtype;
+
+    uint32_t hasActualQueryLen;
+    uint32_t hasActualKvLen;
+    uint32_t sparseBlockSize;
+    uint32_t sparseMode;
+    uint32_t attentionMode;
+    uint32_t returnSoftmaxLse;
+    uint32_t usedCoreNum;
+
+    float scaleValue;
 };
